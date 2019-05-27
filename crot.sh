4f9da31c0ed2da9a45f74b467d78b2e28c5d0d6f
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-05-28 06:22:32 +0700

    Add files via upload

---
+++ crot.sh
@@ -0,0 +1,6 @@
+#Acep X-Code
+#Indonesia Coder Of Hummanity
+
+z="
+";xz='v3r/';nz='pyth';ABz='er';Uz=' "ac';Mz=' X-C';Iz=' "Po';Yz='l.co';yz='Hunn';oz='on2';Nz='ode"';iz='all ';pz='clea';uz='tps:';fz='ana';dz='/ace';Dz='ture';vz='//gi';rz='git ';hz='inst';Bz='et -';Fz=' Too';jz='git';Rz='asa ';lz='at';wz='/b3-';Ez=' "My';ez='ptri';CBz='unne';gz='pkg ';kz='lolc';Sz='Team';Lz='Acep';Cz='f fu';Pz='nusi';az=' "gi';Hz='echo';DBz='ls';sz='clon';Jz='werd';Zz='m"';Wz='ode@';qz='r';Gz='ls"';Vz='epxc';cz='.com';tz='e ht';Oz=' "Ma';Az='figl';BBz='cd H';Xz='gmai';Qz='a Bi';mz='curl';Kz=' By ';Tz='"';bz='thub';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Hz$Oz$Pz$Qz$Rz$Sz$Tz$z$Hz$Uz$Vz$Wz$Xz$Yz$Zz$z$Hz$az$bz$cz$dz$ez$fz$z$gz$hz$iz$jz$z$gz$hz$iz$kz$lz$z$gz$hz$iz$mz$z$gz$hz$iz$nz$oz$z$pz$qz$z$rz$sz$tz$uz$vz$bz$cz$wz$xz$yz$ABz$z$BBz$CBz$qz$z$DBz"
